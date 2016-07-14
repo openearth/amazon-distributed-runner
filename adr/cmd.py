bcd598fc271fcd9d6ccaf4b3bb31dbda8d1aa923
Bas Hoonhout <bas@hoonhout.com>
2016-07-14 17:11:08 +0200

    Initial, empty commit

---
+++ adr/cmd.py
@@ -0,0 +1,3 @@
+def adr():
+
+    print 'Hello world!'
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Amazon Distributed Runner',
+    version='0.0',
+    author='Bas Hoonhout',
+    author_email='b.m.hoonhout@tudelft.nl',
+    packages=find_packages(),
+    description='A distributed scheduling system based on Amazon Web Services',
+    install_requires=[
+        'boto3',
+        'fabric',
+    ],
+    #setup_requires=[
+    #    'sphinx',
+    #    'sphinx_rtd_theme'
+    #],
+    entry_points={'console_scripts': [
+        'adr = adr.cmd:adr',
+    ]},
+)
